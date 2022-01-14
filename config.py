c3a47f103e2c1945c5793c1d568e7c3f0920fda9
ptlhrs7 <90519283+ptlhrs7@users.noreply.github.com>
2022-01-14 08:09:48 +0530

    Additional Python program Files

---
+++ config.py
@@ -0,0 +1,3 @@
+#  Add your API key
+yelp_key = "o0GsiJOyBi5FOIGZIYa9SPY98HGAwY_In4oQaZESttqUl-144oF6XWmd_lDT-rBSKdN8u2X_At7nBLaeVm5EHNlExwMpLmme-sQQgErH0RbjIdSy_IILytlea3reYXYx"
+g_key = "AIzaSyDFIxvsaRCjNMJ7Rxo3X37WukBZS6h-I74"
